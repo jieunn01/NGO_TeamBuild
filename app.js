69db2eff67c33641f0e3997ad5d5dca2ac69f015
jieun <bje0825@naver.com>
2022-01-11 14:26:33 -0500

    fixed port issue

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-//const http = require('http');
 import { createServer } from 'http';
-
-const hostname = '127.0.0.1';
-const port = 3000;
+ 
+const port = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1> THis is really easy! </h1>');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>This is really easy!</h1>');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running at port: ${port}/`);
 });
