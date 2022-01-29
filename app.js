b54ceaaa0ea3492b6a57441f5dab1b17d3c1396a
jieun <bje0825@naver.com>
2022-01-28 22:02:01 -0500

    settings prepare

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 import express from 'express';
-import router from "./routes/index.js";
+import router from './routes/index.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// set up the root directory reference
+// find the global URL using fileURLToPath
+// and then turn that into the __dirname (something like Users/Desktop/NGO_Redux/)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-//set up the static assets
-app.use(express.static(path.join(__dirname, 'public'))); // href="css/main.css"
+app.use(express.static(path.join(__dirname, "public")));
 
-// getting routes paramers (like php GET and POST superglobals)
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// these are our main routes
+// tell app to use the router file
 app.use('/', router);
 
-// use this route for anything that doesn't match 
-app.use(function(req, res) {
-    //routes doesn't match, probably a 404 or something 
-    console.log('page does not exist');
-    res.sendFile(path.join(__dirname, '/views/404.html'));
-})
 app.listen(port, () => {
-    console.log(`Server running at port: ${port}/`);
+    console.log(`App is running at ${port}/`);
 });
